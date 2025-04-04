06b3eeb71e0d8a9421ad0ceab1ddb7df7ba73a5c
PatrykOzog <104310014+PatrykOzog@users.noreply.github.com>
2025-04-04 21:07:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,9 +22,9 @@ setup(
 	    # and there is a pip3 conflict if we have the following
 	    # line.
         #'gammatone @ git+https://github.com/detly/gammatone',
-        #'pypesq @ git+https://github.com/vBaiCai/python-pesq',
+        'pypesq @ git+https://github.com/ludlows/python-pypesq',
         'srmrpy @ git+https://github.com/jfsantos/SRMRpy',
-        #'pesq @ git+https://github.com/ludlows/python-pesq',
+        'pesq @ git+https://github.com/ludlows/python-pesq',
     ],
     include_package_data=True
 )
